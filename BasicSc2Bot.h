e61f7ffa714026f25ef3529cb0fc332df4b92a7b
Richard Wu <richard9@ualberta.ca>
2024-10-14 12:14:52 -0600

    tutorial + test

--- BasicSc2Bot.h
+++ BasicSc2Bot.h
@@ -6,11 +6,26 @@
 #include "sc2lib/sc2_lib.h"
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
+#include "sc2api/sc2_unit.h"
+#include "cpp-sc2/include/sc2api/sc2_interfaces.h"
+#include "sc2api/sc2_unit_filters.h"
+
+using namespace sc2;
 
 class BasicSc2Bot : public sc2::Agent {
 public:
 	virtual void OnGameStart();
+	virtual void OnGameEnd();
 	virtual void OnStep();
+
+    void OnUnitIdle(const Unit *unit);
+    bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
+    bool TryBuildSupplyDepot();
+    bool TryBuildBarracks();
+    size_t CountUnitType(UNIT_TYPEID unit_type);
+    void Scout(const Unit* unit);
+
+    const Unit* FindNearestMineralPatch(const Point2D& start);
 
 private:
 };
