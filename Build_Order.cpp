e374d1506ac9916b4284e7de648e502ea08c3dbe
Youngwoo Choi <cyan246010@gmail.com>
2024-10-22 01:03:21 -0600

    Update Build_Order.cpp
    Build Fusion Core (issue 05)

--- Build_Order.cpp
+++ Build_Order.cpp
@@ -2,12 +2,16 @@
 
 using namespace sc2;
 
+
 void BasicSc2Bot::ExecuteBuildOrder() {
 	BuildBarracks();
 	BuildFactory();
     BuildStarport();
+	BuildTechLabAddon();
+	BuildFusionCore();
 }
 
+// Build Barracks if we have a supply depot and enough resources
 void BasicSc2Bot::BuildBarracks() {
     const ObservationInterface* observation = Observation();
 
@@ -19,6 +23,7 @@ void BasicSc2Bot::BuildBarracks() {
 	}
 }
 
+// Build factory if we have a barracks and enough resources
 void BasicSc2Bot::BuildFactory() {
 	const ObservationInterface* observation = Observation();
 
@@ -29,6 +34,7 @@ void BasicSc2Bot::BuildFactory() {
 	}
 }
 
+// Build Starport if we have a factory and enough resources
 void BasicSc2Bot::BuildStarport() {
 	const ObservationInterface* observation = Observation();
 
@@ -36,5 +42,23 @@ void BasicSc2Bot::BuildStarport() {
 	Units starports = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_STARPORT));
 	if (!factories.empty() && starports.empty() && (observation->GetMinerals() >= 150 && observation->GetVespene() >= 100)) {
 		TryBuildStructure(ABILITY_ID::BUILD_STARPORT, UNIT_TYPEID::TERRAN_SCV);
+	}
+}
+
+void BasicSc2Bot::BuildTechLabAddon() {
+	const ObservationInterface* observation = Observation();
+
+	Units factories = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_FACTORY));
+
+}
+
+// Build Fusion Core if we have a starport and enough resources
+void BasicSc2Bot::BuildFusionCore() {
+	const ObservationInterface* observation = Observation();
+
+	Units starports = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_STARPORT));
+	Units fusioncore = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_FUSIONCORE));
+	if (!starports.empty() && fusioncore.empty() && (observation->GetMinerals() >= 150 && observation->GetVespene() >= 150)) {
+		TryBuildStructure(ABILITY_ID::BUILD_FUSIONCORE, UNIT_TYPEID::TERRAN_SCV);
 	}
 }
